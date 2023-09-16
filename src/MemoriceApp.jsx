32bbd450889802c6ff370b18d411b6e0919e063f
FernandoLorca <florcapinto@gmail.com>
2023-09-16 10:34:07 -0300

    Layout responsive done

--- src/MemoriceApp.jsx
+++ src/MemoriceApp.jsx
@@ -4,27 +4,17 @@ export default function MemoriceApp() {
       <section>
         <h1 className="text-center text-4xl py-10">🧠 Memorice App</h1>
       </section>
-      <section className="grid grid-cols-5 px-3 grid-rows-4 place-items-center gap-2">
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
-        <div className="w-full h-32 rounded-md text-xl bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
+      <section className="grid grid-cols-5 px-3 grid-rows-4 place-items-center gap-2 md:gap-y-10">
+        <div className="w-full h-32 rounded-md text-xl md:w-28 bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
+        <div className="w-full h-32 rounded-md text-xl md:w-28 bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
+        <div className="w-full h-32 rounded-md text-xl md:w-28 bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
+        <div className="w-full h-32 rounded-md text-xl md:w-28 bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
+        <div className="w-full h-32 rounded-md text-xl md:w-28 bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
+        <div className="w-full h-32 rounded-md text-xl md:w-28 bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
+        <div className="w-full h-32 rounded-md text-xl md:w-28 bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
+        <div className="w-full h-32 rounded-md text-xl md:w-28 bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
+        <div className="w-full h-32 rounded-md text-xl md:w-28 bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
+        <div className="w-full h-32 rounded-md text-xl md:w-28 bg-slate-700 flex justify-center items-center cardPattern cursor-pointer"></div>
       </section>
     </>
   );
