f96b3bcd1010625cf8a7f108177478cba995ed10
FernandoLorca <florcapinto@gmail.com>
2023-09-17 11:53:37 -0300

    Change cards patterns and edit display of title

--- src/components/CardPattern.jsx
+++ src/components/CardPattern.jsx
@@ -1,7 +1,7 @@
 export default function CardPattern({ handleShowCard, flipped, cardId }) {
   return (
     <div
-      className={`w-full h-32 rounded-md text-xl md:w-28 justify-center items-center cardPattern cursor-pointer ${
+      className={`w-full border-2 border-gray-900 h-32 rounded-md text-xl md:w-28 justify-center items-center cardPattern cursor-pointer ${
         flipped ? 'hidden' : 'flex'
       }`}
       onClick={() => handleShowCard(cardId)}
