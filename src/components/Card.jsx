a527f5821f40892e8c10f96106092346333cba4f
FernandoLorca <florcapinto@gmail.com>
2023-09-21 08:44:04 -0300

    Change styles and display for cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card({
 }) {
   return (
     <div
-      className={`w-full border h-32 rounded-md text-xl md:w-28 justify-center items-center cursor-pointer ${
+      className={`border-2 border-gray-900 w-32 h-40 rounded-md text-4xl flex justify-center items-center cursor-pointer bg-gray-950 ${
         flipped ? 'flex' : 'hidden'
       }`}
       onClick={() => flippedCardsHandler(cardId)}
