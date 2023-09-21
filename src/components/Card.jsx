231e9d15afe08779bf6699a11f616406f98201c0
FernandoLorca <florcapinto@gmail.com>
2023-09-21 08:27:04 -0300

    Flipped card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,15 @@
-export default function Card({ handleShowCard, cardContent, flipped }) {
+export default function Card({
+  cardId,
+  flippedCardsHandler,
+  cardContent,
+  flipped,
+}) {
   return (
     <div
       className={`w-full border h-32 rounded-md text-xl md:w-28 justify-center items-center cursor-pointer ${
         flipped ? 'flex' : 'hidden'
       }`}
-      onClick={handleShowCard}
+      onClick={() => flippedCardsHandler(cardId)}
     >
       {cardContent}
     </div>
