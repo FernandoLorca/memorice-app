2bf79d07cb0b9aca54d1c9ebfbe00b87ec407795
FernandoLorca <florcapinto@gmail.com>
2023-09-26 12:44:05 -0300

    Footer done

--- src/MemoriceApp.jsx
+++ src/MemoriceApp.jsx
@@ -1,11 +1,13 @@
-import CardGrid from './components/CardGrid'
 import TitleApp from './components/TitleApp'
+import CardGrid from './components/CardGrid'
+import Footer from './components/Footer/Footer'
 
 export default function MemoriceApp() {
   return (
     <>
       <TitleApp />
       <CardGrid />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/components/Creators.jsx
@@ -0,0 +1,3 @@
+export default function Creators({ text }) {
+  return <span className="text-2xl font-bold text-rose-400">{text}</span>
+}
